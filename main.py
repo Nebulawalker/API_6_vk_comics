d6c6670dcc64f4be1ef53f6b4887f69c60a6e54c
Roman Okatev <nebulawalker@yandex.ru>
2022-09-12 21:38:22 +0300

    Добавлен функционал для удаления временных файлов, даже в случае непредвиденной ошибки.

--- main.py
+++ main.py
@@ -9,9 +9,18 @@ def main():
     load_dotenv()
     access_token = os.getenv("ACCESS_TOKEN")
     vk_group_id = os.getenv("VK_GROUP_ID")
-    image_filepath, comic_commentary = get_random_xkcd_comic()
-    publish_post(access_token, vk_group_id, image_filepath, comic_commentary)
-    os.remove(image_filepath)
+    try:
+        image_filepath, comic_commentary = get_random_xkcd_comic()
+
+        publish_post(
+            access_token,
+            vk_group_id,
+            image_filepath,
+            comic_commentary
+        )
+
+    finally:
+        os.remove(image_filepath)
 
 
 if __name__ == "__main__":
